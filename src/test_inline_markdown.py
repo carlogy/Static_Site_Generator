37648c8058b9834bef63ea9d124a6240a28e2943
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-03-22 09:14:20 -0400

    added unit tests to test functionality of the inline_markdown.py split_nodes_delimiter function.

---
+++ src/test_inline_markdown.py
@@ -0,0 +1,95 @@
+import unittest
+
+from inline_markdown import (
+    split_nodes_delimiter
+)
+
+from textnode import TextNode
+
+class TestInlineMarkdown(unittest.TestCase):
+    def test_delim_not_in_text(self):
+        node = TextNode("None formatted text", "text")
+        self.assertEqual(
+            split_nodes_delimiter([node], "*", "text"),
+            [TextNode("None formatted text", "text")]
+        )
+
+    def test_bold_delim(self):
+        node = TextNode("This is **bold**.", "text")
+        self.assertListEqual(
+            [
+                TextNode("This is ", "text"),
+                TextNode("bold", "bold"),
+                TextNode(".", "text")
+            ],
+            split_nodes_delimiter([node], "**", "bold")
+        )
+
+    def test_multiple_bold_within_text(self):
+        node = TextNode("This is **bold** and so is **this**", "text")
+        self.assertListEqual(
+            [
+                TextNode("This is ", "text"),
+                TextNode("bold", "bold"),
+                TextNode(" and so is ", "text"),
+                TextNode("this", "bold")
+            ],
+            split_nodes_delimiter([node], "**", "bold")
+        )
+
+    def test_invalid_input(self):
+        node = TextNode("This has **bad markdown", "text")
+        self.assertRaises(
+            Exception,
+            msg="Invalid Markdown syntax: The provided node does not have the correct Markdown syntax."
+
+        )
+
+    def test_non_text_node(self):
+        node = [
+            TextNode("This is **bold**", "text"),
+            "Non-TextNode object",
+            TextNode("**This too**", "text")
+        ]
+
+        self.assertListEqual(
+            [
+                TextNode("This is ", "text"),
+                TextNode("bold", "bold"),
+                "Non-TextNode object",
+                TextNode("This too", "bold")
+            ],
+            split_nodes_delimiter(node, "**", "bold")
+        )
+
+    def text_code_block_node(self):
+        node = TextNode("This is sentance with a `code block` word", "text")
+
+        self.assertListEqual(
+            [
+                TextNode("This is sentance with a ", "text"),
+                TextNode("code block", "code"),
+                TextNode(" word", "text"),
+            ],
+            split_nodes_delimiter([node], "`", "code")
+        )
+
+    def test_italic_node(self):
+        node = TextNode("This is a sentence with an *italic* word", "text")
+
+        self.assertListEqual(
+            [
+                TextNode("This is a sentence with an ", "text"),
+                TextNode("italic", "italic"),
+                TextNode(" word", "text"),
+            ],
+            split_nodes_delimiter([node], "*", "italic"),
+        )
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
