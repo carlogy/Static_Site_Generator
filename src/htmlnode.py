011265fc2a821ee19e0b337a34b2e0287ab9321f
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-04-10 07:23:23 -0400

    define LeafNode child class to HTMLNode. This class represent nested html tag elements. Also added unittesting for the to_html method of that class in test_htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -8,11 +8,30 @@ class HTMLNode:
     def to_html(self):
         raise NotImplementedError
     def props_to_html(self):
+        if self.props is None:
+            return ""
         if self.props is not None:
             props_list = []
             for key, value in self.props.items():
                 props_list.append(f" {key}=\"{value}\"")
             return "".join(props_list)
-        return None
+
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props) -> None:
+        super().__init__(tag, value, props)
+        self.tag = tag
+        self.value = value
+        self.props = props
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf nodes require a value")
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,13 +1,13 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode  import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_paragraph_node(self):
         node = HTMLNode("p", "This is a paragraph")
         self.assertEqual(
             node.props_to_html(),
-            None
+            ""
         )
         self.assertEqual(
             repr(node),
@@ -36,8 +36,38 @@ class TestHTMLNode(unittest.TestCase):
         )
         self.assertEqual(
             node.props_to_html(),
-            None
+            ""
         )
+    def test_no_tag_leaf(self):
+        leafnode = LeafNode(None,"This is just raw text",None)
+        self.assertEqual(
+            leafnode.to_html(),
+            "This is just raw text"
+        )
+    def test_paragraph_leaf(self):
+        leafnode = LeafNode("p", "This is a paragraph", None)
+        self.assertEqual(
+            leafnode.to_html(),
+            "<p>This is a paragraph</p>"
+        )
+    def test_anchor_link_leaf(self):
+        leafnode = LeafNode("a", "Click me!", {'href': 'https://www.google.com', 'target': '_blank'})
+        self.assertEqual(
+            leafnode.to_html(),
+            '<a href="https://www.google.com" target="_blank">Click me!</a>'
+        )
+    def test_no_value_leaf(self):
+        leafnode = LeafNode("p",None,None)
+        with self.assertRaises(ValueError):
+            leafnode.to_html()
+
+    def test_repr(self):
+        leafnode = LeafNode("p", "This is a paragraph", None)
+        self.assertEqual(
+            repr(leafnode),
+            "LeafNode(p, This is a paragraph, None)"
+        )
+
 
 
 if __name__ == "__main__":
