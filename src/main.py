f67e3520cee0cedc8fbee19ca3cab879c3ce86d5
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-04-28 07:40:16 -0400

    defined copy_directory function that takes a file directory as a path and copies all of it's contents into the ./public folder.

--- src/main.py
+++ src/main.py
@@ -14,34 +14,39 @@ def main():
 
 
     def copy_directory(path):
+        public_path = "./public"
+        if os.path.exists(public_path):
+            shutil.rmtree(public_path)
+        os.mkdir(public_path)
         if not os.path.exists(path):
             raise IsADirectoryError("The directory does not exist, or system permissions for this directory are not granted.\n Run ls -l (dir_path) to view permission.\nTo modify use command chomd.")
         folder_structure = {}
 
         if os.path.exists(path):
-            print(f"The path exists! {path}")
 
-            with os.scandir(path) as scanned_directory:
-                print(type(scanned_directory))
+            static_tree = os.walk(path, topdown=True)
 
+            list_structure = list(static_tree)
 
-                list_of_scanned_dir = sorted(scanned_directory, key=lambda entry: entry.is_dir(),)
+            for dir, dir_names, file_names in list_structure:
+                for child_dir in dir_names:
+                    if dir_names in os.listdir(public_path):
+                        continue
+                    if len(dir.removeprefix("./").split("/")) == 1:
+                        os.mkdir(os.path.join(public_path, child_dir))
+                    else:
+                        parent_dir = dir.removeprefix("./static")
+                        path_to_create = f"{public_path}{parent_dir}/{child_dir}"
+                        os.mkdir(path_to_create)
+                        print(os.path.exists(path_to_create))
+                for file in file_names:
+                    if len(dir.removeprefix("./").split("/")) == 1:
+                        shutil.copy(os.path.join(dir,file), os.path.join(public_path))
+                    else:
+                        parent_dir = dir.removeprefix("./static")
+                        path_to_create = f"{public_path}{parent_dir}/{file}"
+                        shutil.copy(os.path.join(dir,file), path_to_create)
 
-                current_public_path = public_path
-                for entry in list_of_scanned_dir:
-                    if entry.is_file():
-                        print(f"{entry.name} is a valid file\nLocated in: {entry.path}\n")
-                        shutil.copy(entry.path, current_public_path)
-                    if entry.is_dir():
-                        print(f"{entry.name} is a directory\nIt's path is: {entry.path}")
-                        with os.scandir(public_path) as child_directory:
-                            if entry.name not in list(child_directory):
-                                print("The directory doesn't exists!")
-
-                                os.mkdir(f"{current_public_path}/{entry.name}")
-                                current_public_path = f"{current_public_path}/{entry.name}"
-                                copy_directory(entry.path)
-
-    # copy_directory(static_path)
+    copy_directory(static_path)
 
 main()
