d9f14a5fdf00c4782975a1de034c914b5d2dccfc
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-04-29 16:57:35 -0400

    moved the copy_directory function to it's own file, updated logging on main.py.

--- src/copy_directory.py
+++ src/copy_directory.py
@@ -4,17 +4,19 @@ import shutil
 
 def copy_directory(path):
     public_path = "./public"
-    os.mkdir(public_path)
-    if not os.path.exists(path):
-        raise IsADirectoryError("The directory does not exist, or system permissions for this directory are not granted.\n Run ls -l (dir_path) to view permission.\nTo modify use command chomd.")
+    if not os.path.exists(public_path):
+        os.mkdir(public_path)
+    if  not os.path.exists(path):
+        raise  FileNotFoundError("The directory does not exist, or system permissions for this directory are not granted.\n Run ls -l (dir_path) to view permission.\nTo modify use command chomd.")
 
     if os.path.exists(path):
         static_tree = os.walk(path, topdown=True)
         list_structure = list(static_tree)
 
         for dir, dir_names, file_names in list_structure:
+            current_public_list = os.listdir(public_path)
             for child_dir in dir_names:
-                if dir_names in os.listdir(public_path):
+                if child_dir in current_public_list:
                     continue
                 if len(dir.removeprefix("./").split("/")) == 1:
                     os.mkdir(os.path.join(public_path, child_dir))
@@ -24,8 +26,10 @@ def copy_directory(path):
                     os.mkdir(path_to_create)
             for file in file_names:
                 if len(dir.removeprefix("./").split("/")) == 1:
+                    print(f"Copying file from {os.path.join(dir,file)} -> {public_path}")
                     shutil.copy(os.path.join(dir,file), os.path.join(public_path))
                 else:
                     parent_dir = dir.removeprefix("./static")
                     path_to_create = f"{public_path}{parent_dir}/{file}"
+                    print(f"Copying file from {os.path.join(dir, file)} -> {path_to_create}")
                     shutil.copy(os.path.join(dir,file), path_to_create)
--- src/main.py
+++ src/main.py
@@ -8,10 +8,10 @@ static_dir_path = "./static"
 
 
 def main():
-    print("Deleting public directory...")
+    print("Deleting public directory...🗑️")
     if os.path.exists(puclic_dir_path):
         shutil.rmtree(puclic_dir_path)
-    print("Copying static files to public directory...")
+    print("Copying static files to public directory...🗄️")
     copy_directory(static_dir_path)
 
 main()
