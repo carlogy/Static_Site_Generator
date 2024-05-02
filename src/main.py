b9d4c54b9b88f7cac79cec7d82db5e33bf65a52d
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-05-02 19:34:50 -0400

    plugged in the extract_title and generate_page functions into the main function and updated main.sh shell script to initiate a python server with public directory as the entry point.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import os
 import shutil
 
 from copy_directory import copy_directory
+from extract_title import extract_title
+from generate_page import generate_page
 
 puclic_dir_path = "./public"
 static_dir_path = "./static"
@@ -13,5 +15,6 @@ def main():
         shutil.rmtree(puclic_dir_path)
     print("Copying static files to public directory...")
     copy_directory(static_dir_path)
+    generate_page("./content/index.md", "./template.html", "./public")
 
 main()
